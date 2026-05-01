fe87ecf4716b5caeb7c5c44d61dedb234cb231dc
Dhruv187 <dhruvgandhi187@gmail.com>
2026-05-01 16:35:25 +0530

    vercel fixes

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://interactive-2-d-combat-engine.vercel.app/",
+    origin: "https://interactive-2-d-combat-engine.vercel.app",
     methods: ["GET", "POST"],
   },
 });
